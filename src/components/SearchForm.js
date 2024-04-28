38f4f7766b259a86572de115ad4fd5c0856514ce
ryk-k <rykk@sheridancollege.ca>
2024-04-27 22:07:47 -0400

    working query

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,37 +1,44 @@
 import React, { useState } from "react";
-import { searchMovies } from "../services/omdbService";
-import MovieList from "./MovieList";
+import { getMovie } from "../services/omdbService";
 
 const SearchForm = () => {
   const [query, setQuery] = useState("");
-  const [results, setResults] = useState([]);
+  const [submittedQuery, setSubmittedQuery] = useState("");
+  const [movie, setMovie] = useState(null);
 
-  const handleSearch = async (e) => {
-    e.preventDefault();
-
-    try {
-        const response = await searchMovies(query);
-        setResults(response);
-    }
-    catch (error) {
-      console.error(error);
-    }
+  const handleChange = (e) => {
+    setQuery(e.target.value);
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setSubmittedQuery(query);
+    const movieData = await getMovie(query);
+    setMovie(movieData);
+  }
 
   return (
     <div>
-      <form onSubmit={handleSearch}>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           value={query}
-          onChange={(e) => setQuery(e.target.value)}
+          onChange={handleChange}
           placeholder="Search for a movie..."
         />
         <button type="submit">Search</button>
       </form>
-<MovieList movies={results} />
+      <p>Showing results for: {submittedQuery}</p>
+
+      {movie && (
+        <div>
+          <h2>{movie.Title}</h2>
+          <img src={movie.Poster} alt={movie.Title} />
+          <p>{movie.Plot}</p>
+        </div>
+      )}
     </div>
   );
-};
+}
 
 export default SearchForm;
